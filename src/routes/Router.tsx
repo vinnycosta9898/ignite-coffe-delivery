6269e7c7a9be9c8e55cbebd0cb263b0459376704
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-05-14 08:26:03 -0300

    Criando o Layout para a aplicacao

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "../pages/Home"
 import { Checkout } from '../pages/Checkout'
+import { DefaultLayout } from "../layouts/DefaultLayout"
 
 export function Router(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/checkout" element={<Checkout/>}/>
+                <Route path="/" element={<DefaultLayout/>}>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/checkout" element={<Checkout/>}/>
+                </Route>
             </Routes>
         </BrowserRouter>
     )
