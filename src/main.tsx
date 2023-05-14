329bd1d2f6d5c7f9aa9b21ac9f3bef2bde367308
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-05-14 08:12:24 -0300

    Criando o GlobalStyle e defaultTheme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/GlobalStyle.ts'
+import { defaultTheme } from './styles/theme/defaultTheme.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
