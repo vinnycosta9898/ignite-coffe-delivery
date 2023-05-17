3f8e0bd5c4ce21ec81f860014876b456be699a01
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-05-17 20:37:00 -0300

    Criando o componente InfoIcon

--- src/components/Typegraphy/styles.ts
+++ src/components/Typegraphy/styles.ts
@@ -23,7 +23,7 @@ export const TitleText = styled.h1<TitleTextProps>`
 export const RegularText = styled.p<RegularTextProps>`
     color: ${({theme, color }) => theme.colors[`base-${color ?? "title"}`]};
     font-size: ${({ theme, size }) => theme.textSizes[`text-regular-${size ?? "m"}`]};
-    font-family: ${({ theme }) => theme.fonts.title};
+    font-family: ${({ theme }) => theme.fonts.regular};
     line-height: 130%;
     font-weight: ${({ weight }) => weight ?? 400}
 `
--- src/pages/Home/components/Intro/index.tsx
+++ src/pages/Home/components/Intro/index.tsx
@@ -1,14 +1,51 @@
-import { IntroContainer, IntroContent } from "./styles"
+import { BenefitsContainer, IntroContainer, IntroContent, IntroTitle } from "./styles"
+import { ShoppingCart, Package, Timer, Coffee } from "phosphor-react" 
 import coffeImg from '../../../../assets/Coffe.svg'
 
+import { useTheme } from "styled-components"
+import { RegularText } from "../../../../components/Typegraphy/styles"
+import { InfoIncon } from "../../../../components/InfoIcon"
+
 export function Intro(){
+    const { colors } = useTheme();
     return(
         <IntroContainer>
             <IntroContent className="container">
                 <div>
                     <section>
-                        
+                        <IntroTitle size="xl">
+                            Encontre o café perfeito para qualquer hora do dia
+                        </IntroTitle>
+                        <RegularText size="l" color="subtitle" as="h3">
+                            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
+                        </RegularText>
                     </section>
+
+                    <BenefitsContainer>
+                        <InfoIncon 
+                            icon={<ShoppingCart weight="fill"/>}
+                            text="Compra simples e segura"
+                            iconBg={colors["brand-yellow-dark"]}
+                        />
+
+                        <InfoIncon 
+                            icon={<Package weight="fill"/>}
+                            text="Embalagem mantém o café intacto"
+                            iconBg={colors["base-text"]}
+                        />
+
+                        <InfoIncon 
+                            icon={<Timer weight="fill"/>}
+                            text="Entrega rápida e rastreada"
+                            iconBg={colors["brand-yellow"]}
+                        />
+
+                        <InfoIncon 
+                            icon={<Coffee weight="fill"/>}
+                            text="O café chega fresquinho até você"
+                            iconBg={colors["brand-purple"]}
+                        />
+                    </BenefitsContainer>
                 </div>
                 <img src={coffeImg} alt="Imagem de café"/>
             </IntroContent>
