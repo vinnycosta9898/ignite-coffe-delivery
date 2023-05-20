0569776e6a6c1a3fd26507d6fd62b046c1c650a2
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-05-19 23:44:22 -0300

    Finalizando a interface da page Checkout

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
     background: ${({ theme }) => theme.colors["brand-yellow"]};
